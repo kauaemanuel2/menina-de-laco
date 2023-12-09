5f72e653dfb62385f6824ecb7e356f94222315c2
kauaemanuel2 <142634462+kauaemanuel2@users.noreply.github.com>
2023-12-08 21:46:23 -0300

    atualização final

--- carrinho.js
+++ carrinho.js
@@ -1,63 +1,79 @@
-// Função para adicionar produtos ao carrinho
-function adicionarAoCarrinho(produtoId) {
-    console.log('Tentativa de adicionar ao carrinho:', produtoId);
 
-    // Recupera os itens do carrinho armazenados no localStorage
+function adicionarAoCarrinho(produtoId) {
+    
     const carrinhoItems = JSON.parse(localStorage.getItem("carrinho")) || [];
 
-    // Verifica se o produto já está no carrinho
+    
     const produtoExistente = carrinhoItems.find(item => item.id === produtoId);
 
     if (produtoExistente) {
-        // Se o produto já estiver no carrinho, aumenta a quantidade
+       
         produtoExistente.quantidade += 1;
     } else {
-        // Se o produto não estiver no carrinho, adiciona um novo item
+       
         carrinhoItems.push({ id: produtoId, quantidade: 1 });
     }
 
-    // Atualiza o localStorage com os itens do carrinho
+    
     localStorage.setItem("carrinho", JSON.stringify(carrinhoItems));
 
-    // Atualiza a exibição dos itens no carrinho
-    atualizarItensNoCarrinho();
+    
+    window.location.href = "carrinho.html";
 }
 
-// Função para atualizar a exibição dos itens no carrinho
-function atualizarItensNoCarrinho() {
+
+function carregarItensDoCarrinho() {
     const carrinhoItems = JSON.parse(localStorage.getItem("carrinho")) || [];
     const carrinhoContainer = document.getElementById("carrinho-items");
 
-    carrinhoContainer.innerHTML = ""; // Limpa o conteúdo atual
+    carrinhoContainer.innerHTML = "";
 
-    if (carrinhoItems.length > 0) {
-        carrinhoItems.forEach(item => {
-            const itemDiv = document.createElement("div");
-            itemDiv.className = "item-carrinho";
-            itemDiv.innerHTML = `
-                <img src="imagens/produto${item.id}.png" alt="Produto ${item.id}">
-                <h3>Produto ${item.id}</h3>
-                <p>Quantidade: ${item.quantidade}</p>
-                <p class="preco">Preço: R$ 29,99</p>
-                <button class="remover-do-carrinho" onclick="removerDoCarrinho('${item.id}')">Remover do Carrinho</button>
+    carrinhoItems.forEach(item => {
+        const produto = obterProdutoPorId(item.id);
+
+        if (produto) {
+            const itemHTML = `
+                <div class="item-carrinho">
+                    <img src="${produto.imagem}" alt="${produto.nome}" width="100px">
+                    <h3>${produto.nome}</h3>
+                    <p>Quantidade: ${item.quantidade}</p>
+                    <p class="preco">R$ ${produto.preco * item.quantidade}</p>
+                    <button class="remover-do-carrinho" onclick="removerDoCarrinho('${item.id}')">Remover do carrinho</button>
+                </div>
             `;
-            carrinhoContainer.appendChild(itemDiv);
-        });
-    } else {
-        carrinhoContainer.innerHTML = "<p>Nenhum item no carrinho.</p>";
-    }
+
+            carrinhoContainer.innerHTML += itemHTML;
+        }
+    });
 }
 
-// Função para remover um item do carrinho
+
+function obterProdutoPorId(produtoId) {
+    
+    const produtos = {
+        produto1: { nome: "Laçinho com boneca", imagem: "imagens/produto1.png", preco: 49.99 },
+        produto2: { nome: "Laço com parzinho", imagem: "imagens/produto2.png", preco: 74.99 },
+        produto3: { nome: "Tiara vermelha", imagem: "imagens/produto3.png", preco: 29.99 },
+        produto4: { nome: "Laço com parzinho vermelho e dourado", imagem: "imagens/produto4.png", preco: 54.99 },
+        produto5: { nome: "Faixa RN", imagem: "imagens/produto5.png", preco: 14.99 },
+    };
+
+    return produtos[produtoId];
+}
+
+
 function removerDoCarrinho(produtoId) {
     const carrinhoItems = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+    
     const novoCarrinho = carrinhoItems.filter(item => item.id !== produtoId);
+
+    
     localStorage.setItem("carrinho", JSON.stringify(novoCarrinho));
 
-    // Atualiza a exibição dos itens no carrinho
-    atualizarItensNoCarrinho();
+    
+    carregarItensDoCarrinho();
 }
 
-// Chama a função de atualizar ao carregar a página
-document.addEventListener("DOMContentLoaded", atualizarItensNoCarrinho);
 
+document.addEventListener("DOMContentLoaded", carregarItensDoCarrinho);
